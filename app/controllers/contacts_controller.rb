c30de6d5e241eafdcdefa059f752eba1ccdf77d5
Cyrille Bonnet <cbonnet99@gmail.com>
2009-03-26 20:49:56 +1300

    Added captcha to newsletter signup

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,12 +6,18 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(params[:contact])
+    if verify_human
     if @contact.save
       flash[:notice] = "Thank you for signing up"
       redirect_back_or_default root_url
     else
       get_districts_and_subcategories
       flash.now[:error] = "Your registration could not be completed"
+      render :action => "new"
+    end
+    else
+      get_districts_and_subcategories
+      flash[:error] = "There was a problem with the words you entered, please try again"
       render :action => "new"
     end
   end
