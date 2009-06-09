134ba2897d32c5e2921ff252da7d6541a3ef7a13
Cyrille Bonnet <cbonnet99@gmail.com>
2009-06-09 12:10:18 +1200

    Fix user rollover

--- public/javascripts/users-rollover.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".user-rollover").mouseover(function(){
-        $(this).css({backgroundColor: '#666', 'border': '1px solid black', cursor: 'pointer'});
-    });
-    $(".user-rollover").mouseout(function(){
-        $(this).css({backgroundColor: '#b3c4d4', 'border': '1px solid white', cursor: 'default'});
-    });
-});
