b959f217f3fd50100b365ef17b053e175fd0353c
Cyrille Bonnet <cbonnet99@gmail.com>
2009-07-09 23:43:46 +1200

    Removed stuff we don't need from HTML editor

--- public/javascripts/mce_editor.js
+++ public/javascripts/mce_editor.js
@@ -1,8 +1,8 @@
 tinyMCE.init({
 mode : "textareas",
 theme : "advanced",
-plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,iespell,searchreplace,print,contextmenu,paste,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
-theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect",
+plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,iespell,print,contextmenu,paste,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+theme_advanced_buttons1 : ",bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect",
 theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,undo,redo",
 theme_advanced_buttons3 : "",
 theme_advanced_toolbar_location : "top",
@@ -11,5 +11,6 @@ theme_advanced_statusbar_location : "bottom",
 theme_advanced_resizing : true,
 extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
 template_external_list_url : "example_template_list.js",
-use_native_selects : true
+use_native_selects : true,
+theme_advanced_blockformats : "p,h1,h2,h3"
 });
