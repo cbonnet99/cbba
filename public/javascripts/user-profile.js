42cf6e8c24d75e19708367e6e7ed71814aa6b71d
Cyrille Bonnet <cbonnet99@gmail.com>
2009-11-08 20:46:40 +1300

    Added new menu for own profile that links directly to content creation

--- public/javascripts/user-profile.js
+++ public/javascripts/user-profile.js
@@ -1,5 +1,26 @@
-$(document).ready(function(){
+var timeout    = 500;
+var closetimer = 0;
 
+function add_content_open()
+{  add_content_canceltimer();
+   add_content_close();
+   $("ul#new-content-menu").css('visibility', 'visible');}
+
+function add_content_close()
+{  $("ul#new-content-menu").css('visibility', 'hidden');}
+
+function add_content_timer()
+{  closetimer = window.setTimeout(add_content_close, timeout);}
+
+function add_content_canceltimer()
+{  if(closetimer)
+   {  window.clearTimeout(closetimer);
+      closetimer = null;}}
+
+$(document).ready(function(){
+	$('#create-new-content').bind('mouseover', add_content_open);
+	$('#create-new-content').bind('mouseout',  add_content_timer);
+	
     $("ul#profile-tabs li").each(
         function(intIndex){
             $(this).hover(
