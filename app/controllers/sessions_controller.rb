2919dd0345bb8f36bd963eae13ae19ad69247ae1
Cyrille Bonnet <cbonnet99@gmail.com>
2009-11-10 15:59:07 +1300

    Still debugging...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,9 +20,9 @@ class SessionsController < ApplicationController
           log_bam_user_event(UserEvent::LOGIN, "", "Success")
           flash.now[:notice] = "Logged in successfully"
           if current_user.admin?
-            redirect_back_or_default reviewer_url(:action => "index", :protocol => APP_CONFIG[:logged_site_protocol])
+            redirect_back_or_default reviewer_url(:action => "index")
           else
-            redirect_back_or_default expanded_user_path(current_user, :protocol => APP_CONFIG[:logged_site_protocol])
+            redirect_back_or_default expanded_user_path(current_user, {:protocol => APP_CONFIG[:logged_site_protocol]})
           end
         else
           @payment = current_user.find_current_payment
