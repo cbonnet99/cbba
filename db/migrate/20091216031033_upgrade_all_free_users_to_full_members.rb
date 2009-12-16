8048865a752d3d5b2c34cdbdb69f38a2fb5c5dae
Cyrille Bonnet <cbonnet99@Ramayana.local>
2009-12-16 19:23:39 +1300

    Added paid features to existing full members before upgrading all free users

--- db/migrate/20091216031033_upgrade_all_free_users_to_full_members.rb
+++ db/migrate/20091216031033_upgrade_all_free_users_to_full_members.rb
@@ -1,8 +1,26 @@
 class UpgradeAllFreeUsersToFullMembers < ActiveRecord::Migration
   def self.up
+    User.full_members.each do |fm|
+      unless fm.name == "Cyrille Bonnet"
+        fm.paid_photo = true
+        fm.paid_photo_until = fm.member_until
+        fm.paid_highlighted = true
+        fm.paid_highlighted_until = fm.member_until
+        fm.paid_special_offers = 1
+        fm.paid_special_offers_next_date_check = fm.member_until
+        fm.paid_gift_vouchers = 1
+        fm.paid_gift_vouchers_next_date_check = fm.member_until
+        puts "==== Adding paid features to #{fm.full_name}"
+        fm.save!
+      end
+    end
+    
     User.free_users.each do |free|
-      free.update_attribute(:free_listing, false)
-      free.add_role("full_member")
+      unless free.name == "Cyrille Bonnet"
+        puts "==== Making #{free.full_name} a full member"
+        free.update_attribute(:free_listing, false)
+        free.add_role("full_member")
+      end
     end
   end
 
