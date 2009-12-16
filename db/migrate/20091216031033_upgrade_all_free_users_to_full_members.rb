a85f7ece1163f3d583997f49aa775489bc8d5f22
Cyrille Bonnet <cbonnet99@Ramayana.local>
2009-12-16 19:55:16 +1300

    Removed paid features, as I had done that earlier... Fixed the upgrade of free listing

--- db/migrate/20091216031033_upgrade_all_free_users_to_full_members.rb
+++ db/migrate/20091216031033_upgrade_all_free_users_to_full_members.rb
@@ -1,25 +1,12 @@
 class UpgradeAllFreeUsersToFullMembers < ActiveRecord::Migration
-  def self.up
-    User.full_members.each do |fm|
-      unless fm.name == "Cyrille Bonnet"
-        fm.paid_photo = true
-        fm.paid_photo_until = fm.member_until
-        fm.paid_highlighted = true
-        fm.paid_highlighted_until = fm.member_until
-        fm.paid_special_offers = 1
-        fm.paid_special_offers_next_date_check = fm.member_until
-        fm.paid_gift_vouchers = 1
-        fm.paid_gift_vouchers_next_date_check = fm.member_until
-        puts "==== Adding paid features to #{fm.full_name}"
-        fm.save!
-      end
-    end
-    
+  def self.up    
     User.free_users.each do |free|
       unless free.name == "Cyrille Bonnet"
         puts "==== Making #{free.full_name} a full member"
-        free.update_attribute(:free_listing, false)
+        free.free_listing = false
+        free.remove_role("free_listing")
         free.add_role("full_member")
+        free.save!
       end
     end
   end
