44769ade92d9634c8bf834275015e28698a0e2dd
Cyrille Bonnet <cbonnet99@Ramayana.local>
2010-06-09 22:49:44 +0200

    If a user tries to sign up for the newsletter, make sure that s/he is signed up for the public newsletter and DO NOT create a contact with that name

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,16 +5,23 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(params[:contact])
     if verify_human
-    if @contact.save
-      flash[:notice] = "Thank you for signing up"
-      redirect_back_or_default root_url
-    else
-      get_districts_and_subcategories
-      flash.now[:error] = "Your registration could not be completed"
-      render :action => "new"
-    end
+      user = params[:contact].nil? ? nil : User.find_by_email(params[:contact]["email"])
+      if user.nil?
+        @contact = Contact.new(params[:contact])
+        if @contact.save
+          flash[:notice] = "Thank you for signing up"
+          redirect_back_or_default root_url
+        else
+          get_districts_and_subcategories
+          flash.now[:error] = "Your registration could not be completed"
+          render :action => "new"
+        end
+      else
+        user.update_attribute(:receive_newsletter, true)
+        flash[:notice] = "Thank you for signing up"
+        redirect_back_or_default root_url
+      end
     else
       get_districts_and_subcategories
       flash[:error] = "There was a problem with the words you entered, please try again"
--- test/functional/contacts_controller_test.rb
+++ test/functional/contacts_controller_test.rb
@@ -8,13 +8,31 @@ class ContactsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:contact)
     assert assigns(:contact).errors.size > 0
   end
-  test "creation success" do
-    post :create, {:contact => {:email => "cyrille@blob.com" }}
-    assert_not_nil assigns(:contact)
-#    puts assigns(:contact).errors.inspect
-    assert_equal 0, assigns(:contact).errors.size
-  end
   
+    test "creation success" do
+      post :create, {:contact => {:email => "cyrille@blob.com" }}
+      assert_not_nil assigns(:contact)
+  #    puts assigns(:contact).errors.inspect
+      assert_equal 0, assigns(:contact).errors.size
+      
+      old_size = Contact.all.size
+      post :create, {:contact => {:email => "cyrille@blob.com" }}
+      assert_equal old_size, Contact.all.size
+    end
+
+    test "creation for member" do
+      cyrille = users(:cyrille)
+      cyrille.receive_newsletter = false
+      cyrille.save!
+      cyrille.reload
+      post :create, {:contact => {:email => cyrille.email }}
+      assert_nil assigns(:contact)
+  #    puts assigns(:contact).errors.inspect
+  
+      cyrille.reload
+      assert cyrille.receive_newsletter
+    end
+
   def test_unsubscribe
     joe = contacts(:joe)
     assert joe.receive_newsletter?
