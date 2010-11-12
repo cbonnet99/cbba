6cf2808aa2b28fbd17321b7458705e3d6819aebc
Cyrille Bonnet <cbonnet99@Ramayana.local>
2010-11-12 10:51:27 +0100

    Fix HTML link for counters

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -2,6 +2,6 @@ class Counter < ActiveRecord::Base
   named_scope :published, :conditions => ["state= 'published'"] 
   
   def controller_name
-    title.gsub(/ /, '_').downcase
+    class_name.underscore.pluralize.downcase
   end
 end
