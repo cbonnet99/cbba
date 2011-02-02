4b26a489edf9e78d0890323ef0d618e815f778fa
Cyrille Bonnet <cbonnet99@Ramayana.local>
2011-02-02 22:57:19 +0100

    Added new text from Sav

--- public/javascripts/add-features.js
+++ public/javascripts/add-features.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
+    
+    $("#buy_now_triple_vis").click(function(event){
+       $("#order_package_triple_vis").attr('checked', 'checked');
+       $("form.edit_order").submit(); 
+    });
+    
+    $("#buy_now_stand_out").click(function(event){
+       $("#order_package_stand_out").attr('checked', 'checked');
+       $("form.edit_order").submit(); 
+    });
+    
+    $("#buy_now_permium").click(function(event){
+       $("#order_package_premium").attr('checked', 'checked');
+       $("form.edit_order").submit(); 
+    });
+    
     $("li").removeClass("checked");
-    $("input:checked").parents("li").addClass("checked");    
+    $("input:checked").parents("li").addClass("checked");
 });
 $(".edit_order li").click(function(){
     $(this).children("input").attr('checked', 'checked');
