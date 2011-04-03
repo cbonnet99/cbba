fbb341e74a82017df42a7c5bc657502eed207c30
Cyrille Bonnet <cbonnet99@Ramayana.local>
2011-04-03 11:24:59 +0200

    Improved statistics page

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -3,11 +3,12 @@ class Admin::StatisticsController < ApplicationController
     @date = params[:date].blank? ? Time.now : Time.parse(params[:date])
     @start_date = @date.beginning_of_week
     @end_date = @date.end_of_week
-    p = Payment.find(:all, :conditions => {:created_at => @start_date..@end_date } )
-    @payments = p.size
-    @total_payments = p.inject(0){|sum, p| sum+p.amount} || 0
-    @signups = User.count(:all, :conditions => {:created_at => @start_date..@end_date } )
-    @published_profiles = UserProfile.count(:all, :conditions => {:published_at => @start_date..@end_date } )
+    @payments = Payment.find(:all, :conditions => {:created_at => @start_date..@end_date, :status => "completed"} )
+    @pending_payments = Payment.find(:all, :conditions => {:created_at => @start_date..@end_date, :status => "pending"} )
+    @total_payments = @payments.inject(0){|sum, p| sum+p.amount} || 0
+    @signups = User.find(:all, :conditions => {:created_at => @start_date..@end_date } )
+    @published_profiles = UserProfile.find(:all, :conditions => {:published_at => @start_date..@end_date, :state => "published"} )
+    @draft_profiles = UserProfile.find(:all, :conditions => {:created_at => @start_date..@end_date, :state => "draft"} )
     @published_articles = Article.count(:all, :conditions => {:published_at => @start_date..@end_date } )
     @published_SOs = SpecialOffer.count(:all, :conditions => {:published_at => @start_date..@end_date } )
     @published_GVs = GiftVoucher.count(:all, :conditions => {:published_at => @start_date..@end_date } )
