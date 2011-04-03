9fa0f6904fdf31e0458ddd7cf73d3d5358243891
Cyrille Bonnet <cbonnet99@Ramayana.local>
2011-04-03 10:57:08 +0200

    Fix bug in admin resident experts

--- test/functional/admin/resident_experts_controller_test.rb
+++ test/functional/admin/resident_experts_controller_test.rb
@@ -2,8 +2,15 @@ require File.dirname(__FILE__) + '/../../test_helper'
 
 class Admin::ResidentExpertsControllerTest < ActionController::TestCase
   def test_index
+    #fixtures: create a user with points
+    subcat = Factory(:subcategory)
+    user = Factory(:user, :subcategory1_id => subcat.id)
+    article = Factory(:article, :author => user, :subcategory1_id => subcat.id, :state => "draft")
+    article.publish!    
+    
     get :index, {}, {:user_id => users(:cyrille).id }
     assert_response :success
+    assert !assigns(:subcategories).blank?
   end
   def test_index_for_subcategory
     get :index_for_subcategory, {:id => subcategories(:yoga).id }, {:user_id => users(:cyrille).id }
