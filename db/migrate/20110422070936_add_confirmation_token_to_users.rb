10c63ce2b401a452ecc81c979cd7f084adfb8729
Cyrille Bonnet <cbonnet99@Ramayana.local>
2011-04-26 21:26:26 +0200

    Removed unused migration

--- db/migrate/20110422070936_add_confirmation_token_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddConfirmationTokenToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :confirmation_token, :string
-  end
-
-  def self.down
-    remove_column :users, :confirmation_token
-  end
-end
