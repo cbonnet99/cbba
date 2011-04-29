289a0648da488158ae629e7df423e15c0679ab54
Cyrille Bonnet <cbonnet99@Ramayana.local>
2011-04-29 15:06:25 +0200

    Fix URLs again...

--- vendor/gems/ambethia-recaptcha-0.2.2/lib/recaptcha.rb
+++ vendor/gems/ambethia-recaptcha-0.2.2/lib/recaptcha.rb
@@ -11,9 +11,9 @@ module Recaptcha
   end
 
   
-  RECAPTCHA_API_SERVER_URL        = 'http://www.google.com/recaptcha/api'
-  RECAPTCHA_API_SECURE_SERVER_URL = 'https://www.google.com/recaptcha/api'
-  RECAPTCHA_VERIFY_URL            = 'http://www.google.com/recaptcha/api/verify'
+  RECAPTCHA_API_SERVER        = 'http://www.google.com/recaptcha/api'
+  RECAPTCHA_API_SECURE_SERVER = 'https://www.google.com/recaptcha/api'
+  RECAPTCHA_VERIFY            = 'http://www.google.com/recaptcha/api/verify'
 
   SKIP_VERIFY_ENV = ['test', 'cucumber']
 
