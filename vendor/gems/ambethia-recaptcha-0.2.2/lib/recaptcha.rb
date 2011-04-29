73ed5f4e5e087f7b4359ee17f8a60bb7ceb54562
Cyrille Bonnet <cbonnet99@Ramayana.local>
2011-04-29 12:05:22 +0200

    Changed Recaptcha URL to go the new Google-based one

--- vendor/gems/ambethia-recaptcha-0.2.2/lib/recaptcha.rb
+++ vendor/gems/ambethia-recaptcha-0.2.2/lib/recaptcha.rb
@@ -12,7 +12,7 @@ module Recaptcha
 
   
   RECAPTCHA_API_SERVER        = 'http://api.recaptcha.net';
-  RECAPTCHA_API_SECURE_SERVER = 'https://api-secure.recaptcha.net';
+  RECAPTCHA_API_SECURE_SERVER = 'https://www.google.com/recaptcha/api';
   RECAPTCHA_VERIFY_SERVER     = 'api-verify.recaptcha.net';
 
   SKIP_VERIFY_ENV = ['test', 'cucumber']
