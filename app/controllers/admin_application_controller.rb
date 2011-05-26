8325fc1539b50bd51fc7818c329a33d398947096
Cyrille Bonnet <cbonnet99@Ramayana.local>
2011-05-26 23:11:52 +0200

    Removed call_rake, rake in settings, changed zingabeam.net to zingabeam.com

--- app/controllers/admin_application_controller.rb
+++ app/controllers/admin_application_controller.rb
@@ -1,13 +1,6 @@
 class AdminApplicationController < SecureApplicationController
   
   before_filter :admin_required
-
-  def call_rake(task, options = {})
-    options[:rails_env] ||= Rails.env
-    args = options.map { |n, v| "#{n.to_s.upcase}='#{v}'" }
-    system "#{APP_CONFIG[:rake]} #{task} #{args.join(' ')} --trace 2>&1 >> #{Rails.root}/log/rake.log &"
-  end
-  
   
   protected
     
