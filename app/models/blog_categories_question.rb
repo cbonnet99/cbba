668219310d6df1fe0105f787a2c5201ce97bd997
Cyrille Bonnet <cbonnet99@Ramayana.(none)>
2011-07-20 22:13:07 +0200

    Removed stuff for questions/answers

--- app/models/blog_categories_question.rb
+++
@@ -1,4 +0,0 @@
-class BlogCategoriesQuestion < ActiveRecord::Base
-  belongs_to :blog_category
-  belongs_to :question
-end
--- app/models/blog_subcategories_question.rb
+++
@@ -1,4 +0,0 @@
-class BlogSubcategoriesQuestion < ActiveRecord::Base
-  belongs_to :blog_subcategory
-  belongs_to :question
-end
