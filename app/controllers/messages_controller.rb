0957128aec468f060a540b98ce2842ce3839461c
Cyrille Bonnet <cbonnet99@localhost.(none)>
2012-02-17 09:45:54 +0100

    Error message in case of spam on message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,14 @@ class MessagesController < ApplicationController
   
   def create
     @user = User.find_active_paying_user(params[:message]["user_id"])
-    my_params = params[:message].dup
-    my_params[:body].gsub!(/\r/, "<br/>")
-    @message = Message.new(params[:message])
-    unless @user.nil?
+    if @user.nil?
+      flash[:error] = "Could not send message"
+      render :action => "new"
+    else
       if logged_in? || verify_human
+        my_params = params[:message].dup
+        my_params[:body].gsub!(/\r/, "<br/>")
+        @message = Message.new(params[:message])
         if @message.save
           UserMailer.deliver_message(@message)
           log_bam_user_event UserEvent::MSG_SENT, "Subject: #{@message.subject}", {}, {:visited_user_id => @message.user.id }
